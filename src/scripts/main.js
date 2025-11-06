58a9c621501590b81b3cff13b2c1f56119fe2eae
Yuliia Voroniuk <juliasemchishin1545@gmail.com>
2025-11-06 23:33:18 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,42 +9,31 @@ document.addEventListener('click', (e) => {
 
   const borderLeftWidth = parseFloat(wallStyle.borderLeftWidth);
   const borderTopWidth = parseFloat(wallStyle.borderTopWidth);
-  const borderRightWidth = parseFloat(wallStyle.borderRightWidth);
-  const borderBottomWidth = parseFloat(wallStyle.borderBottomWidth);
 
   const left = wallRect.left + borderLeftWidth;
   const top1 = wallRect.top + borderTopWidth;
-  const right = wallRect.right - borderRightWidth;
-  const bottom = wallRect.bottom - borderBottomWidth;
 
-  if (
-    e.clientX < left ||
-    e.clientX > right ||
-    e.clientY < top1 ||
-    e.clientY > bottom
-  ) {
-    return;
+  let coordX = e.clientX - left - spider.clientWidth / 2;
+  let coordY = e.clientY - top1 - spider.clientHeight / 2;
+
+  if (coordX < 0) {
+    coordX = 0;
   }
 
-  const coordX = e.clientX - left;
-  const coordY = e.clientY - top1;
+  if (coordY < 0) {
+    coordY = 0;
+  }
 
-  const insideWidth = right - left;
-  const insideHeight = bottom - top1;
+  if (coordX > wallRect.width - spider.clientWidth) {
+    coordX = wallRect.width - spider.clientWidth - borderLeftWidth * 2;
+  }
 
-  let spiderCenterX = coordX - spider.clientWidth / 2;
-  let spiderCenterY = coordY - spider.clientHeight / 2;
+  if (coordY > wallRect.height - spider.clientHeight) {
+    coordY = wallRect.height - spider.clientHeight - borderLeftWidth * 2;
+  }
 
-  spiderCenterX = Math.max(
-    0,
-    Math.min(spiderCenterX, insideWidth - spider.clientWidth),
-  );
-
-  spiderCenterY = Math.max(
-    0,
-    Math.min(spiderCenterY, insideHeight - spider.clientHeight),
-  );
-
-  spider.style.left = `${spiderCenterX}px`;
-  spider.style.top = `${spiderCenterY}px`;
+  if (e.target === wall) {
+    spider.style.top = coordY + 'px';
+    spider.style.left = coordX + 'px';
+  }
 });
